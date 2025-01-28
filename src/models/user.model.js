fb3a0464188375d01be6b6fcf6350710fe47a659
yashkataraei <yashkataraei@gmail.com>
2025-01-28 10:59:22 +0530

    file-upload

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from "mongoose";
+import { JsonWebTokenError } from "jsonwebtoken";
+import bcrypt from bcrypt;
 const userSchema = new Schema(
     {
         
@@ -51,6 +53,41 @@ const userSchema = new Schema(
 
 
 )
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password") )
+        return next()
+    this.password=bcrypt.hash(this.password,10)
+ 
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await  bcrypt.compare(password,this.password)
+}
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User",userSchema)
